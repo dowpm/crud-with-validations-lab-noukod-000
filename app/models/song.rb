8b96c26257e2a840b55a40e12f06a1333276b336
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-09 04:56:46 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
 
   def released?
     released
