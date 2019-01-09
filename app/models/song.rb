d6f447b8f5623360bdadaac929e2d8b23caaef53
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-09 04:11:55 +0000

    Automatically backed up by Learn

---
+++ app/models/song.rb
@@ -0,0 +1,10 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {
+   scope: [:artist_name, :release_year],
+   message: "cannot be repeated by the same artist in the same year"
+  }
+
+  validates :released, inclusion: { in: %w(true false) }
+
+end
