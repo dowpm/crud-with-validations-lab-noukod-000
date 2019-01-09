4175d808a1070655a7c27910b1b9ef966cd9096c
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-09 04:21:20 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,16 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update]
   def index
     @songs = Song.all
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def set_song
+    @song = Song.find params[:id]
   end
 end
