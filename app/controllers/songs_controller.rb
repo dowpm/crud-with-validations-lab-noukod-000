a980862d036f0f1e2df030b093dd38e31b57dafd
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-09 04:35:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,8 +13,12 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create song_params
-    redirect_to song_path(@song)
+    @song = Song.new song_params
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
